e931231712fceadbc55cacd9bec3068dee611146
Max Walter <mwa@ad.neos-it.de>
2019-08-27 12:21:39 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div id="app" class="container mx-auto flex flex-col h-screen justify-center">
+
+    <div class="p-3 w-1/2 mx-auto">
+      <img src="./NeosIT-Logo 2017.png" alt="" id="logo" width="200">
+      <div class="row my-4">
+        <date-picker v-model="value" :lang="lang" :first-day-of-week="1" range format="DD.MM.YYYY" class="w-4/6 border-none outline-none"></date-picker>
+        <input type="text" placeholder="Ausbildungs Woche" class="w-3/12 h-8 border rounded float-right p-1 border-gray-400" v-model="woche">
+      </div>
+
+      <textarea name="content" id="" cols="30" rows="10" class="border rounded w-full p-3 border-gray-400" placeholder="Bericht einfügen" v-model="text"></textarea>
+      <button class="bg-blue-400 w-full my-3 text-white text-xl font-medium p-3 rounded" @click="create"><span class="p-3">Bericht erstellen</span></button>
+
+    </div>
+
+
+    
+    <img src="./banner.png" alt="" id="banner" width="600" style="display: none">
+  </div>
+</template>
+
+<script>
+
+import DatePicker from 'vue2-datepicker';
+import jspdf from 'jspdf';
+import base64Image from 'base64-img';
+import { constants } from 'crypto';
+
+export default {
+  name: 'app',
+  components: { DatePicker },
+  data() {
+    return {
+      value: '',
+      woche: '',
+      text: '',
+      // custom lang
+      lang: {
+        days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        months: ['Januar', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      },
+    }
+  },
+  methods: {
+    create: function() {
+      var image = document.getElementById('logo');
+      var banner = document.getElementById('banner');
+
+      console.log(banner);
+      var doc = new jspdf({
+        orientation: 'p',
+        unit: 'px',
+        format: 'a4'
+      });
+
+      doc.addImage(image, 'PNG', 225, 35, 200, 48);
+      doc.addImage(banner, 'PNG', 80, 500, 266, 18);
+
+
+      doc.setFontStyle("bold");
+      doc.setFontSize(12);
+
+      doc.text('Ausbildungsnachweis', 330, 110);
+      doc.setFontStyle("normal");
+      doc.setFontSize(12);
+
+
+
+      doc.text(`${this.woche}.Ausbildungswoche | NeosIT | vom ${this.dateReturn(this.value[0], this.value[1])}`, 425, 125, {align:'right'});
+      doc.text(`BBS2 Wolfsburg | Max Walter | Ausbildungsjahr: 1`, 425, 135, {align:'right'})
+
+      doc.setFontStyle("bold");
+      doc.setFontSize(14);
+      doc.text('Tätigkeiten:', 50, 180);
+
+      doc.setFontStyle("normal");
+      doc.setFontSize(12);
+
+      doc.text(this.text, 50, 200, {align:'left'});
+      doc.save("Tescht");
+
+
+    },
+    dateReturn: function(firstDate, secondDate) {
+      /* this.dateReturn(this.value[0], this.value[1]) */
+      var FirstNewDate = new Date(Date.parse(firstDate));
+      var SecondNewDate = new Date(Date.parse(secondDate));
+      return (FirstNewDate.getDate() + "." + (FirstNewDate.getMonth() + 1) + "." + FirstNewDate.getFullYear()) + " bis " + (SecondNewDate.getDate() + "." + (SecondNewDate.getMonth() + 1) + "." + SecondNewDate.getFullYear());
+    }
+  },
+  
+}
+</script>
