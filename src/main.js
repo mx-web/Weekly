4c9d7d4694059db2a3d54f88f29552456cc67799
Max Walter <mwa@ad.neos-it.de>
2019-11-25 09:00:03 +0100

    Prevent window from closing

--- src/main.js
+++ src/main.js
@@ -6,5 +6,10 @@ Vue.config.productionTip = false
 require('tailwindcss/dist/tailwind.css');
 
 new Vue({
-  render: h => h(App),
+	render: h => h(App),
+	mounted() {
+		window.onbeforeunload = function(){
+			return "Are you sure you want to close the window?";
+		}
+	}
 }).$mount('#app')
